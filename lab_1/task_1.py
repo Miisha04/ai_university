5775b3018f6681977fd23913edd59fd87941228f
Michael <misha.gladkov.04@gmail.com>
2025-03-06 13:14:22 +0300

    task1 | lab1

---
+++ lab_1/task_1.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+#5variant
+# Создайте с использованием NumPy массив из 60 элементов (от 10 до 70), пере-
+# форматируйте в формат (6 × 10), выполните слайсинг с шагом 2 по первой оси
+# и 3 по второй
+
+start = 10
+end = 70
+arr = np.array(range(start, end))
+arr = arr.reshape((6,10))
+dlina_posl = 100_000_000
+
+result = arr[::2, ::3]
+
+print(result)
